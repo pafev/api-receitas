32440d13d1e986d9b891083ec077844a8256389f
pafev <paulofvilarimda@gmail.com>
2025-04-24 23:39:49 -0300

    feat(persistence): add sqlite connection

---
+++ src/ApiReceitas/Persistence/SqLiteDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ApiReceitas.Domain.Models;
+
+namespace ApiReceitas.Persistence
+{
+    public class SqLiteDbContext: DbContext
+    {
+        public DbSet<Recipe>? Recipes { get; set; }
+        public DbSet<Ingredient>? Ingredients { get; set; }
+
+        public SqLiteDbContext(DbContextOptions<SqLiteDbContext> options): base(options)
+        {
+        }
+    }
+}
