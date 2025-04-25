a60964a5f4be8e88b375d24dfbaa051271a38ae7
pafev <paulofvilarimda@gmail.com>
2025-04-25 00:03:29 -0300

    feat(program): add services with scoped lifetime

--- src/ApiReceitas/Program.cs
+++ src/ApiReceitas/Program.cs
@@ -1,10 +1,13 @@
 using ApiReceitas.Persistence;
 using Microsoft.EntityFrameworkCore;
+using ApiReceitas.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     var dbConn = builder.Configuration.GetConnectionString("DbConnection");
     builder.Services.AddDbContext<SqLiteDbContext>(options => options.UseSqlite(dbConn));
+    builder.Services.AddScoped<IRecipesService, RecipesService>();
+    builder.Services.AddScoped<IIngredientsService, IngredientsService>();
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
