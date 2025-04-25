10bf4108258601aa36bcbf8b0f6e915676cbfa3d
pafev <paulofvilarimda@gmail.com>
2025-04-25 00:04:39 -0300

    fix: fix tabs in code

--- src/ApiReceitas/Controllers/IngredientsController.cs
+++ src/ApiReceitas/Controllers/IngredientsController.cs
@@ -5,59 +5,58 @@ using ApiReceitas.Services;
 namespace ApiReceitas.Controllers
 {
     [ApiController]
-[Route("api/[controller]")]
-public class IngredientsController : ControllerBase
-{
-    private readonly IIngredientsService _ingredientsService;
-
-    public IngredientsController(IIngredientsService ingredientsService)
+    [Route("api/[controller]")]
+    public class IngredientsController : ControllerBase
     {
-        _ingredientsService = ingredientsService;
-    }
+        private readonly IIngredientsService _ingredientsService;
 
-    [HttpGet]
-    public async Task<IActionResult> GetAllIngredients()
-    {
-        var ingredients = await _ingredientsService.GetAllIngredientsAsync();
-        return Ok(ingredients);
-    }
-
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetIngredientById(Guid id)
-    {
-        var ingredient = await _ingredientsService.GetIngredientByIdAsync(id);
-        if (ingredient == null)
+        public IngredientsController(IIngredientsService ingredientsService)
         {
-            return NotFound();
-        }
-        return Ok(ingredient);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> AddIngredient([FromBody] Ingredient ingredient)
-    {
-        await _ingredientsService.AddIngredientAsync(ingredient);
-        return CreatedAtAction(nameof(GetIngredientById), new { id = ingredient.IngredientId }, ingredient);
-    }
-
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateIngredient(Guid id, [FromBody] Ingredient ingredient)
-    {
-        if (id != ingredient.IngredientId)
-        {
-            return BadRequest();
+            _ingredientsService = ingredientsService;
         }
 
-        await _ingredientsService.UpdateIngredientAsync(ingredient);
-        return NoContent();
-    }
+        [HttpGet]
+        public async Task<IActionResult> GetAllIngredients()
+        {
+            var ingredients = await _ingredientsService.GetAllIngredientsAsync();
+            return Ok(ingredients);
+        }
 
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteIngredient(Guid id)
-    {
-        await _ingredientsService.DeleteIngredientAsync(id);
-        return NoContent();
-    }
-}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetIngredientById(Guid id)
+        {
+            var ingredient = await _ingredientsService.GetIngredientByIdAsync(id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+            return Ok(ingredient);
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> AddIngredient([FromBody] Ingredient ingredient)
+        {
+            await _ingredientsService.AddIngredientAsync(ingredient);
+            return CreatedAtAction(nameof(GetIngredientById), new { id = ingredient.IngredientId }, ingredient);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateIngredient(Guid id, [FromBody] Ingredient ingredient)
+        {
+            if (id != ingredient.IngredientId)
+            {
+                return BadRequest();
+            }
+
+            await _ingredientsService.UpdateIngredientAsync(ingredient);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteIngredient(Guid id)
+        {
+            await _ingredientsService.DeleteIngredientAsync(id);
+            return NoContent();
+        }
+    }
 }
--- src/ApiReceitas/Controllers/RecipesController.cs
+++ src/ApiReceitas/Controllers/RecipesController.cs
@@ -5,58 +5,58 @@ using ApiReceitas.Services;
 namespace ApiReceitas.Controllers
 {
     [ApiController]
-[Route("api/[controller]")]
-public class RecipesController : ControllerBase
-{
-    private readonly IRecipesService _recipesService;
-
-    public RecipesController(IRecipesService recipesService)
+    [Route("api/[controller]")]
+    public class RecipesController : ControllerBase
     {
-        _recipesService = recipesService;
-    }
+        private readonly IRecipesService _recipesService;
 
-    [HttpGet]
-    public async Task<IActionResult> GetAllRecipes()
-    {
-        var recipes = await _recipesService.GetAllRecipesAsync();
-        return Ok(recipes);
-    }
-
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetRecipeById(Guid id)
-    {
-        var recipe = await _recipesService.GetRecipeByIdAsync(id);
-        if (recipe == null)
+        public RecipesController(IRecipesService recipesService)
         {
-            return NotFound();
-        }
-        return Ok(recipe);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> AddRecipe([FromBody] Recipe recipe)
-    {
-        await _recipesService.AddRecipeAsync(recipe);
-        return CreatedAtAction(nameof(GetRecipeById), new { id = recipe.RecipeId }, recipe);
-    }
-
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateRecipe(Guid id, [FromBody] Recipe recipe)
-    {
-        if (id != recipe.RecipeId)
-        {
-            return BadRequest();
+            _recipesService = recipesService;
         }
 
-        await _recipesService.UpdateRecipeAsync(recipe);
-        return NoContent();
-    }
+        [HttpGet]
+        public async Task<IActionResult> GetAllRecipes()
+        {
+            var recipes = await _recipesService.GetAllRecipesAsync();
+            return Ok(recipes);
+        }
 
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteRecipe(Guid id)
-    {
-        await _recipesService.DeleteRecipeAsync(id);
-        return NoContent();
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRecipeById(Guid id)
+        {
+            var recipe = await _recipesService.GetRecipeByIdAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            return Ok(recipe);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRecipe([FromBody] Recipe recipe)
+        {
+            await _recipesService.AddRecipeAsync(recipe);
+            return CreatedAtAction(nameof(GetRecipeById), new { id = recipe.RecipeId }, recipe);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateRecipe(Guid id, [FromBody] Recipe recipe)
+        {
+            if (id != recipe.RecipeId)
+            {
+                return BadRequest();
+            }
+
+            await _recipesService.UpdateRecipeAsync(recipe);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecipe(Guid id)
+        {
+            await _recipesService.DeleteRecipeAsync(id);
+            return NoContent();
+        }
     }
-}
 }
