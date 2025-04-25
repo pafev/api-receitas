77c14b4ca77de66fc8706ad372b0e8ab5c5a8211
pafev <paulofvilarimda@gmail.com>
2025-04-25 00:25:26 -0300

    fix(domain): fix nullable properties

--- src/ApiReceitas/Domain/Models/IngredientsModel.cs
+++ src/ApiReceitas/Domain/Models/IngredientsModel.cs
@@ -3,7 +3,7 @@ namespace ApiReceitas.Domain.Models
     public class Ingredient
     {
         public Guid IngredientId { get; set; }
-        public string? Nome { get; set; }
-        public string? Unit { get; set; }
+        public string Nome { get; set; }
+        public string Unit { get; set; }
     }
 }
--- src/ApiReceitas/Domain/Models/RecipesModel.cs
+++ src/ApiReceitas/Domain/Models/RecipesModel.cs
@@ -3,7 +3,7 @@ namespace ApiReceitas.Domain.Models
     public class Recipe
     {
         public Guid RecipeId { get; set; }
-        public string? Method { get; set; }
-        public Ingredient[]? Ingredients { get; set; }
+        public string Method { get; set; }
+        public Ingredient[] Ingredients { get; set; }
     }
 }
