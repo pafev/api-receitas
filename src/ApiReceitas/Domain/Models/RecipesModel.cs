715e1b5f9636149294bff62eae6eadee0565bc4a
pafev <paulofvilarimda@gmail.com>
2025-04-24 23:39:29 -0300

    feat(domain): add recipes model

---
+++ src/ApiReceitas/Domain/Models/RecipesModel.cs
@@ -0,0 +1,9 @@
+namespace ApiReceitas.Domain.Models
+{
+    public class Recipe
+    {
+        public Guid RecipeId { get; set; }
+        public string? Method { get; set; }
+        public Ingredient[]? Ingredients { get; set; }
+    }
+}
